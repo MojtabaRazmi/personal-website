f409bd24207d942585272e7ab27bf86341b47490
unknown <razmimoj@gmail.com>
2018-12-11 18:55:15 +0330

    20

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,44 +1,71 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
+import mineRouting from './../configRouting';
+import toast from 'react-toastify';
 
-const Login = ({history}) => {
-    const handleLogin =()=>{
-        history.replace('/')
+class Login extends Component {
+    state={
+        email:'',
+        password:''
     };
-    return (
-        <form className='rtl form-signin border rounded m-2 mx-auto bg-light shadow'>
-            <h1 className='h3 mb-3 font-weight-normal'>
-                لطفا وارد شوید
-            </h1>
-            <label for="inputEmail" className='sr-only'>
-                آدرس ایمیل
-            </label>
-            <input
-                type='email'
-                id='inputEmail'
-                className='form-control'
-                placeholder='آدرس ایمیل'
-                required
-                autoFocus
-            />
-            <label for="inputPassword" className='sr-only'>
-                کلمه عبور
-            </label>
-            <input
-                type='password'
-                id='inputPassword'
-                className='form-control'
-                placeholder='کلمه عبور'
-                required
-            />
-            <button
-                className='btn btn-lg btn-primary btn-block'
-                type='submit'
-                onClick={handleLogin}
-            >
-                ورود
-            </button>
-        </form>
-    );
-};
+
+    handleSubmit = async e =>{
+        e.preventDefault();
+        try{
+            const {data} = await axios.post(mineRouting.api_login,
+                JSON.parse(JSON.stringify(this.state))
+            );
+
+            localStorage.setItem('token',data);
+            this.props.history.push('/admin')
+        }
+        catch (e) {
+            if(e.response && e.response===400)
+                toast.error('ایمیل یا پسورد اشتباه است')
+        }
+    };
+
+    render() {
+        return (
+            <form
+                onSubmit={this.handleSubmit}
+                className='rtl form-signin border rounded m-2 mx-auto bg-light shadow'>
+                <h1 className='h3 mb-3 font-weight-normal'>
+                    لطفا وارد شوید
+                </h1>
+                <label for="inputEmail" className='sr-only'>
+                    آدرس ایمیل
+                </label>
+                <input
+                    type='email'
+                    id='inputEmail'
+                    className='form-control'
+                    placeholder='آدرس ایمیل'
+                    required
+                    autoFocus
+                    onChange={(e)=>this.setState({email : e.target.value})}
+                />
+                <label for="inputPassword" className='sr-only'>
+                    کلمه عبور
+                </label>
+                <input
+                    type='password'
+                    id='inputPassword'
+                    className='form-control'
+                    placeholder='کلمه عبور'
+                    required
+                    onChange={(e)=>this.setState({password : e.target.value})}
+                />
+                <button
+                    className='btn btn-lg btn-primary btn-block'
+                    type='submit'
+                    onClick={this.handleLogin}
+                >
+                    ورود
+                </button>
+            </form>
+        );
+    }
+}
 
 export default Login;
